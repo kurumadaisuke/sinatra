53ca69abb1782399f06d466ca4c9e7eb12b36659
kurumadaisuke <kuruma0909@gmail.com>
2023-05-02 20:46:04 +0900

    メモアプリ完成。rubocop,ERB Llntクリア。

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # sinatra
+## メモアプリ導入手順
+1)メモアプリ導入ディレクトリの作成,移動
+```
+base ❯ mkdir memoapp
+base ❯ cd memoapp
+```
+
+2)git cloneによるファイルのダウンロード
+```
+base ❯ git clone https://github.com/kurumadaisuke/sinatra.git
+```
+
+3)Gemfileによるインストール
+```
+base ❯ bundle install
+```
+
+4)メモアプリ起動
+```
+base ❯ bundle exec ruby memoapp.rb
+```
+
+ブラウザにてメモアプリの起動を確認
+http://localhost:4567/
--- memoapp.rb
+++ memoapp.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
-require "sinatra/json"
+require 'sinatra/json'
+require 'cgi'
 
 set :environment, :production
 
@@ -20,11 +23,11 @@ post '/new' do
   title = params[:title]
   content = params[:content]
   id = (memos.keys.max.to_i + 1).to_s
-  new_memo = {"title" => title, "content" => content}
+  new_memo = { 'title' => title, 'content' => content }
   memos[id] = new_memo
 
-  File.open(json_file, "w") do |file|
-    JSON.dump(memos,file)
+  File.open(json_file, 'w') do |file|
+    JSON.dump(memos, file)
   end
 
   redirect '/'
@@ -42,8 +45,8 @@ delete '/:id' do
   memos = JSON.parse(File.read(json_file))
   memos.delete(params[:id])
 
-  File.open(json_file, "w") do |file|
-    JSON.dump(memos,file)
+  File.open(json_file, 'w') do |file|
+    JSON.dump(memos, file)
   end
 
   redirect '/'
@@ -62,10 +65,10 @@ patch '/edit/:id' do
   edit_title = params[:title]
   edit_content = params[:content]
 
-  memos[params[:id]] = {"title" => edit_title, "content" => edit_content}
+  memos[params[:id]] = { 'title' => edit_title, 'content' => edit_content }
 
-  File.open(json_file, "w") do |file|
-    JSON.dump(memos,file)
+  File.open(json_file, 'w') do |file|
+    JSON.dump(memos, file)
   end
 
   redirect "/show/#{params[:id]}"
